c72be36173a6f771b55d10cc3e1d5bd6d9882023
nicat.zeynalli <nicat.zeynalli@digigo.work>
2026-03-02 17:01:34 +0400

    enhance: minor changes in mock data

--- app/src/main/java/com/example/crocusoft_project1/data/PostRepositoryImpl.kt
+++ app/src/main/java/com/example/crocusoft_project1/data/PostRepositoryImpl.kt
@@ -18,11 +18,11 @@ class PostRepositoryImpl: PostRepository {
             .map {
             PostEntity(
                username =  it.toString(),
-                contents = listOf("https://picsum.photos/id/18/200/300"),
+                contents = listOf("https://picsum.photos/200/300"),
                 location = "Baku, Azerbaijan",
                 description = "A beautiful sunset over the mountains in Azerbaijan. Follow me to stay tuned with latest updates ",
                 date = "14 December",
-                userUri = "https://mockimage.tw/photo/320x240/ffffff/000000/$it"
+                userUri = "https://i.pinimg.com/236x/07/01/66/070166623e835c5421388e5278ca8c9a.jpg"
             )
         }
     }
